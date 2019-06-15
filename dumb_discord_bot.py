2b73d43492086bfe76f6ba1d89d5bbe7239cab3c
Avd2000 <avadh23p@gmail.com>
2019-06-14 17:51:18 -0700

    Changed what bot does
    bot now replies with "HOT STUFF IS COMING" when it sees a message that starts with hot

--- dumb_discord_bot.py
+++ dumb_discord_bot.py
@@ -1,4 +1,5 @@
 import discord
+import dbl
 
 READY_STATUS = "Discord Bot now online!"
 
@@ -7,13 +8,14 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("Logged into discord!")
-    await client.change_presence(game=discord.Game(READY_STATUS))
 
 @client.event
 async def on_message(message):
-    if message.author.id == "134482734771994624":
-        await client.send_message(message.author, "I love scadman!")
+    print(message.content)
+    if message.content.startswith('hot'):
+        channel = message.channel
+        await channel.send('HOT STUFF IS COMING')
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     with open("discord.key", "r") as f:
         client.run(f.readline())
